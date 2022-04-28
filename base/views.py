f3e52e7fde2737c8fd401a57decdd7173f4d2cc8
Bekzhan Alakbayev <bekaffee@gmail.com>
2022-04-28 19:39:10 +0600

    [ADD] styling pages

--- base/views.py
+++ base/views.py
@@ -26,7 +26,6 @@ class CustomLoginView(LoginView):
 class RegisterPage(FormView):
     template_name = 'base/register.html'
     form_class = UserCreationForm
-    # redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -49,6 +48,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        
+        context['search_input'] = search_input
         return context
 
 
@@ -56,6 +61,11 @@ class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['task'] = context['task'].filter(user=self.request.user)
+    #     return context
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
